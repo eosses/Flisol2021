b61954cddb8eff52a229f5be53bcc38cb3fd38fa
Evandro Osses <evandrofelipeosses@gmail.com>
2021-04-25 15:53:44 -0300

    Update main.tf

--- Container/main.tf
+++ Container/main.tf
@@ -1,1 +1,14 @@
+#Criação da Imagem
+resource "docker_image" "image_id" {
+  name = "pengbai/docker-supermario:latest"
+}
 
+# Inicia o Container
+resource "docker_container" "container_id" {
+  name  = "supermario"
+  image = "${docker_image.image_id.latest}"
+  ports {
+    internal = "8080"
+    external = "80"
+  }
+}
