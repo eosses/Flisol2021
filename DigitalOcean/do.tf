59e6bc9ec901efe4a733b9cfce9c1a777eebe937
Evandro Osses <evandrofelipeosses@gmail.com>
2021-04-25 15:49:02 -0300

    Update do.tf
    export DO_PAT="Valor do Token" Será utilizado como expressão para preenchimento do Token.
    
    O input da variável var.do_token será preenchido na execução do terraform apply -var "do_token=${DO_PAT}"

--- DigitalOcean/do.tf
+++ DigitalOcean/do.tf
@@ -1,1 +1,27 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+}
+}
+}
+variable "do_token" {}
 
+provider "digitalocean" {
+    token = var.do_token
+}
+
+resource "digitalocean_droplet" "ubuntu" {
+  name     = "Ubuntu1"
+  region   = "nyc1"
+  size     = "s-1vcpu-1gb"
+  image    = "ubuntu-20-04-x64"
+  ssh_keys = ["30108618"]
+}
+resource "digitalocean_droplet" "centOS" {
+  name     = "centOS2"
+  region   = "nyc1"
+  size     = "s-1vcpu-1gb"
+  image    = "ubuntu-20-04-x64"
+  ssh_keys = ["30108618"]
+}
